6a58d545cb736dbe119b8b1691da3567d16b276f
melkotoury <melkotoury@gmail.com>
2018-09-25 17:31:57 +0200

    feat: add dashboard and clients components

---
+++ src/components/clients/Clients.js
@@ -0,0 +1,13 @@
+import React, {Component, Fragment} from 'react';
+
+class Clients extends Component{
+    render(){
+        return(
+            <Fragment>
+                <h1>Clients</h1>
+            </Fragment>
+        )
+    }
+}
+
+export default Clients;
---
+++ src/components/layout/Dashboard.js
@@ -0,0 +1,11 @@
+import React, {Fragment} from 'react';
+import Clients from '../clients/Clients';
+import Sidebar from '../layout/Sidebar';
+
+export default () => {
+    return (
+        <Fragment>
+            <h1>Dashboard</h1>
+        </Fragment>
+    )
+}
